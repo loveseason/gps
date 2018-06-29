7fc607f67ed3278cb5f26eec08fc3da50e664cf0
Paul <loveseasonxiao@gmail.com>
2018-06-29 20:22:47 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace gps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开GPS原始数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(587, 30);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(989, 585);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(49, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "生成IN文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(49, 169);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "打开OUT文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(49, 220);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 29);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "生成NY格式文件";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(49, 268);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(181, 29);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "生成GPS格式文件";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(49, 416);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(530, 199);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "请看使用说明，HAHA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 378);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "README";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1659, 700);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace gps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string DegreeToDecimel(string Degree)        //将度分秒格式转换为小数格式，用来生成in文件
+        {
+            char[] sep = new char[] { '\'', '\"', '°', '.' };
+            string[] dms = Degree.Split(sep);
+            String result = dms[0] + '.' + dms[1] + dms[2] + dms[3];
+            return result;
+
+        }
+        //struct WGS      //导出的84文件的数据结构
+        //{
+        //    public String PName;
+        //    public String WGS_lat;
+        //    public String WGS_lon;
+        //    public String WGS_hgt;
+
+        //}
+        public String[] PointName;
+        public String[] WGS_latitude;
+        public String[] WGS_longtitude;
+        public String[] WGS_height;
+
+        public String[] Code;
+        public String[] X;
+        public String[] Y;
+        public String[] LocalHeight;
+        public String[] H;
+
+
+
+        public int inFileLength;
+        public int out_file_lenth;
+        public String NY_file_path;
+        public String TMP_file_path;                                            //方便以后平均数计算用的，会删除的。
+        public String GPS_file_path;
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)                 //打开84处理为in文件
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    String FullPathName = openFileDialog1.FileName;
+                    String[] readLines = File.ReadAllLines(openFileDialog1.FileName, Encoding.Default);
+                    //用enconding.default来打开windows默认的ANSI格式，防止读取°时出现乱码；
+                    //WGS myWGS;
+                    string inPath = Path.GetDirectoryName(FullPathName) + '\\' + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "-in.txt";
+                    NY_file_path = Path.GetDirectoryName(FullPathName) + '\\' + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "-NY.txt";
+                    TMP_file_path = Path.GetDirectoryName(FullPathName) + '\\' + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "-TMP.txt";
+                    GPS_file_path = Path.GetDirectoryName(FullPathName) + '\\' + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "-GPS.txt";
+                    //File.Create(inPath);
+
+                    StreamWriter sw = File.CreateText(inPath);                  //新建in文件
+                    String str = null;
+                    inFileLength = readLines.Length;
+                    PointName = new String[inFileLength];
+                    WGS_height = new String[inFileLength];
+                    WGS_latitude = new String[inFileLength];
+                    WGS_longtitude = new String[inFileLength];
+                    Code = new String[inFileLength];
+                    X = new String[inFileLength];
+                    Y = new String[inFileLength];
+                    LocalHeight = new String[inFileLength];
+
+                    for (int i = 0; i < readLines.Length; i++)
+                    {
+                        richTextBox1.Text += str + readLines[i] + Environment.NewLine;
+
+                        Char[] charsep = new char[] { '\t' };
+                        String[] tmp = readLines[i].Split(charsep, StringSplitOptions.RemoveEmptyEntries);   //split选项去掉空白项；
+
+                        //此处添加判定是否有异常行的代码
+
+                        PointName[i] = tmp[0];
+                        Code[i] = tmp[1];
+                        X[i] = tmp[2];
+                        Y[i] = tmp[3];
+                        LocalHeight[i] = tmp[4];
+                        WGS_latitude[i] = tmp[5];
+                        WGS_longtitude[i] = tmp[6];
+                        WGS_height[i] = tmp[7];
+
+
+                        String WGSLine = PointName[i] + '\t' + DegreeToDecimel(WGS_latitude[i]) + '\t' + DegreeToDecimel(WGS_longtitude[i]) + '\t' +
+                            WGS_height[i];
+
+                        //StreamWriter sw = new StreamWriter(inPath);
+                        //StreamWriter sw = File.Open(inPath, FileMode.Open);
+                        sw.WriteLine(WGSLine);
+                    }
+                    sw.Close();
+                    MessageBox.Show("OOOOOOK");
+                }
+            }
+
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)                      //显示in文件
+        {
+            richTextBox1.Text = null;
+            for (int i = 0; i < PointName.Length; i++)
+            {
+                richTextBox1.Text += PointName[i] + '\t' + DegreeToDecimel(WGS_latitude[i]) + '\t' + DegreeToDecimel(WGS_longtitude[i]) + '\t' +
+                        WGS_height[i] + Environment.NewLine;
+
+            }
+            MessageBox.Show("THIS IS IT :)");
+        }
+
+        private void button3_Click(object sender, EventArgs e)                  //打开out文件对H赋值
+        {
+            richTextBox1.Text = null;
+            OpenFileDialog opg = new OpenFileDialog();
+            opg.Filter = "文本文件|*.txt";
+            if (opg.ShowDialog() == DialogResult.OK)
+            {
+                String[] readLines = File.ReadAllLines(opg.FileName, Encoding.Default);
+                out_file_lenth = readLines.Length;
+                H = new string[out_file_lenth];
+                if (out_file_lenth != inFileLength + 1)
+                    MessageBox.Show("in文件与out文件不对应！");
+                else
+                {
+                    for (int i = 1; i < readLines.Length; i++)
+                    {
+                        richTextBox1.Text += readLines[i] + Environment.NewLine;
+
+                        Char[] charsep = new char[] { '\t', ' ' };
+                        String[] tmp = readLines[i].Split(charsep, StringSplitOptions.RemoveEmptyEntries);   //split选项去掉空白项；
+
+                        //此处添加判定是否有异常行的代码
+                        H[i-1] = tmp[3];
+
+                    }
+                    MessageBox.Show(H[out_file_lenth - 2]);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)              //生成内业格式文件
+        {
+            richTextBox1.Text = null;
+            try
+            { 
+                StreamWriter sw = File.CreateText(NY_file_path);                  //新建NY文件
+                for (int i = 0; i < inFileLength; i++)
+                {
+                    sw.WriteLine(Code[i] + '/' + X[i] + '/' + Y[i] + '/' + H[i]);
+                }
+                sw.Close();
+
+                richTextBox1.Text = File.ReadAllText(NY_file_path);
+
+                MessageBox.Show("NY已经生成:)");
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public class MyData
+        {
+            public string Name;
+
+            public int Count;
+
+            public double MeanX;
+
+            public double MeanY;
+
+            public double MeanZ;
+
+            public MyData(string str)
+            {
+                Name = str;
+            }
+            public void Add(double x, double y, double z)
+            {
+                if (Count == 0)
+                {
+                    MeanX = x;
+                    MeanY = y;
+                    MeanZ = z;
+                    Count = 1;
+                }
+                else
+                {
+                    MeanX = (MeanX * Count + x) / (Count + 1);
+                    MeanY = (MeanY * Count + y) / (Count + 1);
+                    MeanZ = (MeanZ * Count + z) / (Count + 1);
+                    Count++;
+                }
+            }
+        }
+
+        //求取重复测绘的互差
+        public class hucha      
+        {
+            public string DisName;
+            public double DisX;
+            public double DisY;
+            public double DisZ;
+
+            public hucha(string str)
+            {
+                DisName = str;
+            }
+
+            public void discha(List<double> lx, List<double> ly, List<double> lz)
+            {
+                DisX = lx.Max() - lx.Min();
+                DisY = ly.Max() - ly.Min();
+                DisZ = lz.Max() - lz.Min();
+            }
+
+            
+
+        }
+
+        public List<Tuple<string, string, double, double, double>> TargetData = new List<Tuple<string, string, double, double, double>>();
+        public List<MyData> Data = new List<MyData>();
+        public List<hucha> myhucha = new List<hucha>();
+        private void button5_Click(object sender, EventArgs e)              //开始生成GPS格式文件
+        {
+            richTextBox1.Text = null;
+
+            try
+            {
+                StreamWriter sw1 = File.CreateText(GPS_file_path);
+                sw1.WriteLine("          青岛市勘察测绘研究院ＧＰＳ观测成果表");
+                sw1.WriteLine("=======================================================");
+                sw1.WriteLine("点名     X(m)            Y(m)          H(m)");
+
+                for (int i = 0; i < inFileLength; i++)
+                {
+                    sw1.WriteLine(Code[i] + '\t' + X[i] + '\t' + Y[i] + '\t' + H[i]);
+                    if (PointName[i].Contains("-"))
+                    {
+                        string[] ss = PointName[i].Split(new char[] { '-' });
+                        TargetData.Add(new Tuple<string, string, double, double, double>(ss[0], ss[1], double.Parse(X[i]), double.Parse(Y[i]), double.Parse(H[i])));
+                    }
+                }
+                if (TargetData.Count != 0)
+                {
+                    List<string> existStr = new List<string>();             //测站的个数 "-"前面一样的字符串,将控制点点名放到一个list中
+                    foreach (var t in TargetData)
+                    {
+                        if (!existStr.Contains(t.Item1))
+                        {
+                            existStr.Add(t.Item1);
+                        }
+                    }
+
+                    for (int j = 0; j < existStr.Count; j++)
+                    {
+                        MyData data = new MyData(existStr[j]);
+                        hucha wohucha = new hucha(existStr[j]);
+                        List<double> tx = new List<double>();
+                        List<double> ty = new List<double>();
+                        List<double> tz = new List<double>();
+                        foreach (var t2 in TargetData)
+                        {
+
+                            if (existStr[j] == t2.Item1)
+                            {
+                                data.Add(t2.Item3, t2.Item4, t2.Item5);
+                                tx.Add(t2.Item3 * 100);
+                                ty.Add(t2.Item4 * 100);
+                                tz.Add(t2.Item5 * 100);
+                            }
+                            wohucha.discha(tx, ty, tz);                     //求取互差
+                        }
+                        Data.Add(data);                             //将需要求平均的数据放到一个list中
+                        myhucha.Add(wohucha);
+                    }
+
+
+                }
+
+                sw1.WriteLine("=======================================================");
+                sw1.WriteLine("注: 平差后的成果为");
+
+                foreach (var d in Data)
+                {
+                    sw1.WriteLine(d.Name + "\t" + d.MeanX.ToString("F3") + "\t" + d.MeanY.ToString("F3") + "\t" + d.MeanZ.ToString("F3"));          // .ToString("F3");  保留三维小数
+                }
+
+
+
+                sw1.WriteLine("=======================================================");
+                sw1.WriteLine("测量:沙海龙      计算:沙海龙      检核:崔现勇");
+                sw1.WriteLine();
+                sw1.WriteLine("仪器:天宝5046455569       日期:2018-");
+                sw1.WriteLine();
+                sw1.WriteLine("备注：H为经青岛市高精度似大地水准面精化的正常高，负值为不要高程。");
+                sw1.WriteLine();
+                sw1.WriteLine("L083(原)	107379.565	239213.737	46.597 ");
+                sw1.WriteLine("		△X=		△Y=		△H=");
+                sw1.Close();
+
+                richTextBox1.Text = File.ReadAllText(GPS_file_path);
+
+                //下面代码用于显示有没有测回超限的数据，限差暂定为3cm
+
+                foreach(var h in myhucha)
+                {
+                    richTextBox2.AppendText("\n");
+                    richTextBox2.AppendText(h.DisName + "\t" + h.DisX.ToString("F3") + "\t" + h.DisY.ToString("F3") + "\t" + h.DisZ.ToString("F3"));
+                    //判断有没有超限
+                    if(h.DisX >= 3 | h.DisY >= 3 | h.DisZ >= 3 )
+                    {
+                        richTextBox2.AppendText("\n");
+                        richTextBox2.AppendText("有超限的数据，请检查" + h.DisName);
+                    }
+                }
+
+                
+
+
+                MessageBox.Show("GPS已生成 :)");
+                //MessageBox.Show("gps已生成！");
+            }
+
+
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+
+    }
+}
+
